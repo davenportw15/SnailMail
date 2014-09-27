0e839cdbaceaf96dc7827c66606c07551f5c60da
William Davenport <willdavenport12@gmail.com>
2014-09-27 14:19:12 -0400

    I'll fix this later.

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ import sqlite3
 import os
 
 from models.users import Users
+from models.mail import Mail
 
 app = Flask(__name__)
 
@@ -11,6 +12,7 @@ db = sqlite3.connect(os.path.abspath("db.sqlite"))
 
 # Models
 users = Users(db)
+mail = Mail(db)
 
 @app.route("/api/mail")
 def api_mail():
@@ -22,7 +24,11 @@ def api_send():
 
 @app.route("/user/<id>")
 def api_users(id):
-  return jsonify(user=users.get_user(int(id)))
+  return jsonify(user=users.get_user(id))
+
+@app.route("/mail/<user>")
+def api_mail_for_user(user):
+   return jsonify(mail=mail.get_mail_for_user(user))
 
 if __name__ == "__main__":
   app.run()
--- models/mail.py
+++ models/mail.py
@@ -1,10 +1,13 @@
+from datetime import datetime
+
 class Mail:
 	
 	def __init__(self, db):
 		self.db = db
 
-	def create_mail(self, content, date_sent, sender, recipient, subject):
+	def create_mail(self, content, date_sent, deliver_date, sender, recipient, subject):
 		cursor = self.db.cursor()
+
 		cursor.execute("insert into mail (content, date_sent, sender, recipient, subject) values (?,?,?,?,?)",
 				(content,
 				date_sent,
@@ -15,10 +18,20 @@ class Mail:
 
 	def get_mail(self, id):
 		cursor = self.db.cursor()
-		cursor.execute("select * from mail where id==?", (id,) )
+		cursor.execute("select content, date_sent, sender, recipient, subject from mail where id==?", (id,) )
+		mail = cursor.fetchone()
+		return {
+					"content":mail[0],
+					"date_sent":mail[1],
+					"sender":mail[2],
+					"recipient":mail[3],
+					"subject":mail[4],
+					"id":mail[5]
+				}
 
-	def get_mail_for_user(name):
-		cursor.execute("select * from mail where recipient==?", (name,))
+	def get_mail_for_user(self, name):
+		cursor = self.db.cursor()
+		cursor.execute("select content, date_sent, sender, recipient, subject, id from mail where recipient==?", (name,))
 		mails = cursor.fetchall()
 		return [
 				{
@@ -27,5 +40,6 @@ class Mail:
 					"sender":omail[2],
 					"recipient":omail[3],
 					"subject":omail[4],
+					"id":omail[5]
 				}
 			 for omail in mails]
