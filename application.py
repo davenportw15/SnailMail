55cf3a82f22baca0ba87d97d72439ce3743f56c2
William Davenport <willdavenport12@gmail.com>
2014-09-27 13:39:11 -0400

    Added to models/users.py

--- application.py
+++ application.py
@@ -1,22 +1,28 @@
-from flask import Flask
-from sqlalchemy import *
+from flask import Flask, jsonify
+import sqlite3
+import os
+
+from models.users import Users
 
 app = Flask(__name__)
 
-# SQLAlchemy configuration
-db = create_engine("postgres://avgxpdgohdmvxj:WlP74JeHYwanbAoWs5kG0e1Jrz@ec2-54-225-135-30.compute-1.amazonaws.com:5432/d623bbr29bp99g")
-metadata = BoundMetaData(db)
-
+# SQLite3 connection
+db = sqlite3.connect(os.path.abspath("db.sqlite"))
 
 # Models
-users = models.Users(metadata)
-mail = models.Users(metadata)
+users = Users(db)
 
-@app.route("/api/mail"):
+@app.route("/api/mail")
+def api_mail():
   pass
 
-@app.route("/api/send"):
+@app.route("/api/send")
+def api_send():
   pass
+
+@app.route("/user/<id>")
+def api_users(id):
+  return jsonify(user=users.get_user(int(id)))
 
 if __name__ == "__main__":
   app.run()
--- models/users.py
+++ models/users.py
@@ -1,15 +1,28 @@
-from sqlalchemy import *
-
 class Users:
-  
-  def __init__(self, metadata):
-    self.metadata = metadata
-    self.table = Table("users", self.metadata,
-      Column("id", Integer, primary_key=True),
-      Column("username", String),
-      Column("password", String),
-      Column("latitude", Numeric),
-      Column("longitude", Numeric)
-    )
 
-  
+  def __init__(self, db):
+    self.db = db
+
+  def create_user(self, username, password, longitude, latitude):
+    cursor = self.db.cursor()
+    cursor.execute(
+      "insert into users (username, password, longitude, latitude) values (?, ?, ?, ?)",
+      (username,
+      password,
+      longitude,
+      latitude)
+    )
+    self.db.commit()
+
+  def get_user(self, id):
+    cursor = self.db.cursor()
+    cursor.execute(
+      "select username, latitude, longitude from users where id == ?",
+      (id,)
+    )
+    data = cursor.fetchone()
+    return {
+      "username": data[0],
+      "latitude": data[1],
+      "longitude": data[2]
+    }
