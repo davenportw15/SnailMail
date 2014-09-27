ca56e9322684f7d756d59226f96b13075169f5ed
GregD <gdfusion@gmail.com>
2014-09-27 13:35:04 -0400

    added mail.py

--- models/mail.py
+++ models/mail.py
@@ -1,14 +1,42 @@
-from sqlalchemy import *
 
 class Mail:
 	
-	def __init__(self, metadata):
-		self.metadata = metadata
-		self.table = Table('mail', metadata,
-				Column('id',Integer, primary_key=True)
-				Column('content', String),
-				Column('date_sent', Date),
-				Column('sender', String),
-				Column('recipient', String),
-				Column('subject', String),
-				)
+	def __init__(self, db):
+		self.db = db
+
+	def create_mail(self, content, date_sent, sender, recipient, subject):
+		cursor = self.db.cursor()
+		cursor.execute("insert into mail (content, date_sent, sender, recipient, subject) values (?,?,?,?,?)",
+				(content,
+				date_sent,
+				sender,
+				recipient,
+				subject) )
+		self.db.commit()
+
+	def get_mail(self, id):
+		cursor = self.db.cursor()
+		cursor.execute("select * from mail where id==?", (id,) )
+
+	def get_mail_for_user(name):
+		cursor.execute("select * from mail where recipient==?", (name,))
+		mails = cursor.fetchall()
+		return [
+				{
+					"content":omail[0],
+					"date_sent":omail[1],
+					"sender":omail[2],
+					"recipient":omail[3],
+					"subject":omail[4],
+				}
+			] for omail in mails
+
+		
+
+
+
+
+
+
+
+
