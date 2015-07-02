62729ceb25c148c2ed83c9e18b91d47454999cef
Gregory Derecho <gdfusion@gmail.com>
2015-07-02 16:28:40 -0400

    Modify users.py, database to use password hashes
    	modified:   ../db.sqlite
    Now this file uses a password hash and a salt
    instead of one password field. In addition,
    the auto increment feature has been turned
    off because it's unneccessary
    
    	modified:   users.py
    Modify this file to use the secure crypto
    hashing algorithm instead of plaintext

--- models/users.py
+++ models/users.py
@@ -1,27 +1,44 @@
+from models import secure_pass
+
 class Users:
 
   def __init__(self, db):
     self.db = db
 
   def create_user(self, username, password, latitude, longitude):
+    sp = secure_pass.Password(password)
+    password_key = sp.get_hash()
+    password_salt = sp.get_salt()
     cursor = self.db.cursor()
     cursor.execute(
-      "insert into users (username, password, latitude, longitude) values (?, ?, ?, ?)",
+      "insert into users (username, password_key, password_salt, latitude, longitude) values (?, ?, ?, ?, ?)",
       (username,
-      password,
+      password_key,
+      password_salt,
       latitude,
       longitude)
     )
+
     self.db.commit()
 
   def user_exists(self, username, password):
     cursor = self.db.cursor()
-    cursor.execute(
-      "select id from users where username == ? and password == ?",
-      (username, password))
-    data = cursor.fetchone()
-    print(data)
-    return not (data == None)
+    # get the salt, key for the username
+    cursor.execute("select password_salt, password_key from users where username == ?",
+            (username,))
+    row = cursor.fetchone()
+    if row is None:
+        return False
+
+    salt = row[0]
+    key = row[1]
+
+    return secure_pass.Password.check_pass(
+            password,
+            key,
+            salt
+            )
+
 
   def get_user(self, id):
     cursor = self.db.cursor()
